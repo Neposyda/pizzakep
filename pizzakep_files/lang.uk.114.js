ec5c39027d3a284a281928fbda65cbaf99b94209
Neposyda <v.guard333@gmail.com>
2020-04-16 08:14:13 +0300

    pizzakep 1

---
+++ pizzakep_files/lang.uk.114.js
@@ -0,0 +1,14 @@
+// Copyright 2006 The Closure Library Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS-IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+window.translations=["Блог","Товари","У цьому блозі немає жодного допису. Почніть, натиснувши кнопку \"Додати допис\".","Ще нема продуктів. Почніть, натиснувши \"Додати товар\".","Немає жодного повідомлення в блозі. Почніть з додаванням сторінки \"блог\".","Ця категорія порожня. Додайте до неї товари або оберіть іншу категорію.","Фотогалерея","В цій фотогалереї не було знайдено зображень.","Безкоштовний","Міні","Стандарт","Профі","Limited"];
